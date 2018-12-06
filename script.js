fab7b641d7516a60c82fa25715597ada86d19344
FlorenceSoudain <43751399+FlorenceSoudain@users.noreply.github.com>
2018-12-06 16:53:12 +0100

    V2

--- script.js
+++ script.js
@@ -8,67 +8,82 @@ var quiz = [
     },
     {
         "question": "Qui a remporté la bataille de Sekigahara ?",
-        "reponse1": "Tokugawa Ieyasu",
+        "reponse1": "Toyotomi Hideyoshi",
         "reponse2": "Oda Nobunaga",
-        "reponse3": "Toyotomi Hideyoshi"
+        "reponse3": "Tokugawa Ieyasu"
     },
     {
-        "question": "Quel célébre samouraï a participé à la bataille de Sekigahara ?",
+        "question": "Quel célèbre samouraï a participé à la bataille de Sekigahara ?",
         "reponse1": "Miyamoto Musashi",
         "reponse2": "Oda Nobunaga",
         "reponse3": "Uesugi Kenshin"
-    }];
+    },
+];
 
-//nombre de question que l'utilisateur a répondu
-var questionRepondu = 0;
+//nombre de question total
+var nbrQuestionRestante = quiz.length;
 
 //nombre de bonnes réponses données par l'utilisateur
 var nbrBonnesReponses = 0;
 
-//récupére la valeur du clic
-var valeur = $('#reponses').val();
-var valeur1 = $('#reponse1').val();
-var valeur2 = $('#reponse2').val();
-var valeur3 = $('#reponse3').val();
-
 //boucle qui parcourt le tableau d'objet
 for (var i = 0; i < quiz.length; i++) {
 
-    //tableau contenant les questions
-    var quest = [quiz[i].question];
-
-    //tableau contenant les réponses du tableau d'objet
-    var rep = [quiz[i].reponse1, quiz[i].reponse2, quiz[i].reponse3];
-
-    //random pour mélanger les réponses
-    var alea = Math.round(Math.random() * 2);
-
     console.log(quiz[i].question);
-    console.log(rep);
     console.log(quiz[i]);
+}
 
-    //variable contenant les bonnes réponses, toujours la réponses index 0
-    var bonneRep = rep[0];
-    console.log("Bonne Réponse : " + bonneRep);
+//tableau contenant les bonnes réponses
+var bonneRep = [quiz[0].reponse1, quiz[1].reponse3, quiz[2].reponse1];
+console.log("Bonne Réponse : " + bonneRep);
 
-    //première question et ses réponses
-    $('#question').html(quiz[i].question);
-    $('#reponse1').html("1 : " + rep[alea]);
-    $('#reponse2').html("2 : " + rep[alea +1]);
-    $('#reponse3').html("3 : " + rep[alea -1]);
+//boucle qui parcourt le tableau des réponses
+for (var j = 0; j < bonneRep.length; j++){
+    console.log('index : ' + j);
+}
+
+var clique = [];
+
+var a = 0;
+
+//première question et ses réponses
+$('#question').html(quiz[a].question);
+$('#reponse1').html("1 : " + quiz[a].reponse1);
+$('#reponse2').html("2 : " + quiz[a].reponse2);
+$('#reponse3').html("3 : " + quiz[a].reponse3);
+
+//récupére ce que l'utilisateur clique
+$('#reponses').click(function () {
+
+    //chaque fois que l'utilisateur répond à une question, nbrQuestionRestante diminue de 1
+    nbrQuestionRestante--;
+    console.log("Question Restante : " + nbrQuestionRestante);
+    fin();
+
+    //récupére la valeur du clic
+    var valeur = $('#reponses').val();
+    var valeur1 = $('#reponse1').val();
+    var valeur2 = $('#reponse2').val();
+    var valeur3 = $('#reponse3').val();
 
     //change la question et les réponses quand on clique sur une réponse, bonne ou mauvaise
-    $('#reponses').click(function () {
+    a++;
+    $('#question').html(quiz[a].question);
+    $('#reponse1').html("1 : " + quiz[a].reponse1);
+    $('#reponse2').html("2 : " + quiz[a].reponse2);
+    $('#reponse3').html("3 : " + quiz[a].reponse3);
 
-        $('#question').html(quiz[i--].question);
-        $('#reponse1').html("1 : " + rep[i]);
-        $('#reponse2').html("2 : " + rep[i + 1]);
-        $('#reponse3').html("3 : " + rep[i -1]);
+    //ajoute la valeur récupérer par le clique de l'utilisateur et l'envoie dans le tableau clique
+    clique.push(valeur1);
+    clique.push(valeur2);
+    clique.push(valeur3);
 
-        questionRepondu++;
-        fin();
-    })
-}
+    //compare le tableau clique avec les entrées du tableau des bonnes réponses
+    if(clique[0] === bonneRep[j]){
+        nbrBonnesReponses++;
+    }
+    console.log("Nombre de bonne réponse : " + nbrBonnesReponses);
+});
 
 //chrono
 var secondes = 0;
@@ -87,25 +102,30 @@ $('#resultat').hide();
 
 //page résultat
 function resultat(){
+    $('#rejouer').click(function () {
+    window.location.reload();
+});
     $('#resultat').show();
     $('#quiz').hide();
     $('#bonnesReponses').html(nbrBonnesReponses);
+    $('#total').html(quiz.length);
     $('#temps').html(minutes + " minute(s) et " + secondes + " secondes");
-    $('#rejouer').click(function () {
-        window.location.reload();
-    })
+
+    var q = 0;
+    var r = 0;
+    for (var u = 0; u < quiz.length; u++){
+        var divs = document.createElement('div');
+        document.getElementById('questionsReponses').appendChild(divs);
+        divs.innerHTML = "Q : " + quiz[q++].question + " R : " + bonneRep[r++];
+    }
+
+
 }
 
-//fonction fin : quand l'utilisateur a répondu à toutes les questions, la page résultat apparait
-function fin(){
-    if (questionRepondu === 3){
+//function de fin de jeu
+function fin() {
+    //quand l'utilisateur a répondu à toutes les questions, la page résultat apparait
+    if (nbrQuestionRestante === 0){
         resultat();
     }
 }
-
-$('#essai').click(function () {
-    nbrBonnesReponses++;
-    questionRepondu++;
-    fin();
-
-});
